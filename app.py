eb0f35c73157fe5be35c8ff78b1860cc5c30c82f
Tyler Huang <thuang00@stuy.edu>
2019-11-20 09:04:06 -0500

    app root functions

---
+++ app.py
@@ -0,0 +1,30 @@
+#Team NSA_Advice
+#huangT, linW, sontagC, zenH
+#SoftDev1 pd2
+#P #01: ArRESTed Development
+#2019-11-2-
+
+from flask import Flask
+app = Flask(__name__)
+app = Flask(__name__)
+app.secret_key = "adsfgt"
+
+session = {}
+
+@app.route("/")
+def root():
+    if 'user' in session:
+        return redirect(url_for("home"))
+    else:
+        return render_template("index.html")
+
+@app.route("/home")
+def home(): #display home page of website
+    if 'user' in session:
+        return render_template("homepage.html")
+    else:
+        return redirect(url_for("root"))
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
