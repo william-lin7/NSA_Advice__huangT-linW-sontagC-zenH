b2154382052e8c81e6dae2fe05cffe2ffa8a4e25
Tyler Huang <thuang00@stuy.edu>
2019-11-22 11:37:58 -0500

    app

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ app = Flask(__name__)
 app.secret_key = "adsfgt"
 
 session = {}
+userInfo = {}
+userID = -1
 
 @app.route("/")
 def root():
@@ -30,6 +32,7 @@ def register():
 
 @app.route("/auth", methods = ["POST"])
 def auth():
+    global userID
     if request.form['submit_button'] == "Sign me up":
         if request.form['password'] != request.form['password2']:
             flash("Error! Passwords do not match")
@@ -56,21 +59,23 @@ def auth():
         dbfile = "data.db"
         db = sqlite3.connect(dbfile)
         c = db.cursor() #above three lines allow sqlite commands to be performed from python script
-        command = "SELECT username FROM users WHERE username = \"{}\";"
+        command = "SELECT * FROM users WHERE username = \"{}\";"
         listUsers = c.execute(command.format(request.form['username'])) #fills in brackets with the given username and executes it in sqlite
         bar = list(enumerate(listUsers))
         if len(bar) > 0: #checks whether there exists a user with the given username
             getPass = "SELECT password FROM users WHERE username = \"{}\";"
-            listPass = c.execute(getPass.format(bar[0][1][0]))
+            listPass = c.execute(getPass.format(bar[0][1][1]))
             for p in listPass:
                 if request.form['password'] == p[0]: #correct username and password
                     session['user'] = request.form['username'] #stores the user in the session
+                    userID = bar[0][1][0]
                     f = open("keys.txt", "r") #opens file with the keys
                     keys = f.readlines()
                     k = keys[0].split(":")
                     #print(k)
                     #print(k[1].strip())
                     session['google_key'] = k[1].strip()
+                    getUserInfo()
                     return redirect(url_for("home"))
                 else:
                     flash("Error! Incorrect password")
@@ -83,13 +88,21 @@ def auth():
 def logout():
     if 'user' in session:
         session.pop('user')
+        userID = -1
     flash("Logout Success!")
     return redirect(url_for("root"))
 
 @app.route("/home")
 def home(): #display home page of website
     if 'user' in session:
-        return render_template("homepage.html", google_key = session['google_key'])
+        return render_template(
+            "homepage.html",
+            google_key = session['google_key'],
+            user = session['user'],
+            name = userInfo['first_name'] + " " + userInfo['last_name'],
+            email = userInfo['email'],
+            pnum = userInfo['phoneNum'],
+            loc = userInfo['location'])
     else:
         return redirect(url_for("root"))
 
@@ -101,6 +114,18 @@ def getTableLen(tbl): #returns the length of a table
     q = c.execute(command.format(tbl))
     for line in q:
         return line[0]
+
+def getUserInfo():
+    dbfile = "data.db"
+    db = sqlite3.connect(dbfile)
+    c = db.cursor()
+    q = c.execute("SELECT * FROM info WHERE id = {}".format(userID))
+    for bar in q:
+        userInfo['first_name'] = bar[1]
+        userInfo['last_name'] = bar[2]
+        userInfo['email'] = bar[3]
+        userInfo['phoneNum'] = bar[4]
+        userInfo['location'] = bar[5]
 
 if __name__ == "__main__":
     app.debug = True
