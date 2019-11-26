9b838d4f17bc36355026786dcf0dfa249a89d6c7
Hilary Zen <hzen00@stuy.edu>
2019-11-26 09:25:55 -0500

    Editing homepage

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ def home(): #display home page of website
     if 'user' in session:
         return render_template(
             "homepage.html",
-            #google_key = session['google_key'],
+            google_key = session['google_key'],
             user = session['user'],
             name = userInfo['firstName'] + " " + userInfo['lastName'],
             email = userInfo['email'],
